d3be5b108eec32307486f9739c09eef76e9a4bbf
NikhilCode12 <promaxchessace@gmail.com>
2024-02-03 04:16:55 +0530

    update: tailwind added

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"> This is the frontend</div>;
+  return (
+    <div className="bg-black text-white text-3xl">
+      <h1>App</h1>
+    </div>
+  );
 }
 
 export default App;
